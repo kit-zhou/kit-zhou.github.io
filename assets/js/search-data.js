9bef9444cf49f80ec54a849ea83d0a7d08c51d8a
kit-zhou <160794303+kit-zhou@users.noreply.github.com>
2025-08-18 22:10:03 +0000

    Deploying to gh-pages from @ kit-zhou/kit-zhou.github.io@a17ef7a762c29b6cf38762dd4a24a3c35aae7daf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,9 +9,9 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+  },{id: "nav-curriculum-vitae",
+          title: "Curriculum Vitae",
+          description: "Press the purple icon to view/download my CV as a PDF.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
