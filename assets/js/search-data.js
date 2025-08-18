7d8e1be1375f488506581c9db372c894ee881f1c
kit-zhou <160794303+kit-zhou@users.noreply.github.com>
2025-08-18 21:44:46 +0000

    Deploying to gh-pages from @ kit-zhou/kit-zhou.github.io@46fdb70f1603d8b6358b28e76407ac219f93b17a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,27 +4,27 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
+          title: "Teaching",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
           },
         },{id: "nav-research",
-          title: "research",
+          title: "Research",
           description: "",
           section: "Navigation",
           handler: () => {
